dac8e83e6c53611b9179ceb11f0d6201bd3e4aca
Spencer Lazzar <spencerlazzar@yahoo.com>
2021-05-05 17:17:06 -0700

    Home Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import image from '../images/paddle.jpg'
+
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <main>
+      <img src={image} alt="paddleImage" className="absolute object-cover w-full h-full" />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-6xl text-black-100 font-bold cursive leading-none lg:leading-snug home-name">Hello. I'm Spencer</h1>
+      </section>
+    </main>
   )
 }
 
